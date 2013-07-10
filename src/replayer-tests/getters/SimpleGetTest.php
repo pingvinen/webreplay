2526d5f21816028b7352f4cc7455dbe20ecc59d0
Patrick <pingvinen@isflagen.dk>
2013-07-10 12:21:49 +0200

    Added POST tests for getters

--- src/replayer-tests/getters/SimpleGetTest.php
+++ src/replayer-tests/getters/SimpleGetTest.php
@@ -58,6 +58,58 @@ class SimpleGetTest extends FixtureBase
 		$last->send();
 		$this->assertEquals($e2content, $last->getResponseBody(), "The response for the last entry is wrong");
 	}
+
+
+
+	/**
+	 * Test simple get replaying with 2 entries.
+	 * Requests are made using HTTP POST
+	 */
+	public function test_valid_using_post()
+	{
+		$streamid = "teststream";
+		$e1content = "{\"name\":\"entry1\",\"int\":1}";
+		$e2content = "{\"name\":\"entry2\",\"int\":2}";
+
+		//
+		// add entry 1
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e1content);
+
+		$add->send();
+
+		//
+		// add entry 2
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e2content);
+
+		$add->send();
+
+
+
+		//
+		// get entry 1
+		//
+		$e1 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_POST);
+		$e1->send();
+		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for the first entry is wrong");
+
+		//
+		// get entry 2
+		//
+		$e2 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_POST);
+		$e2->send();
+		$this->assertEquals($e2content, $e2->getResponseBody(), "The response for the second entry is wrong");
+
+		//
+		// get last entry (should be entry 2)
+		//
+		$last = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_POST);
+		$last->send();
+		$this->assertEquals($e2content, $last->getResponseBody(), "The response for the last entry is wrong");
+	}
 }
 
 ?>
--- src/replayer-tests/getters/SpecificGetTest.php
+++ src/replayer-tests/getters/SpecificGetTest.php
@@ -72,6 +72,72 @@ class SpecificGetTest extends FixtureBase
 		$e1->send();
 		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for entryid 1 is wrong");
 	}
+
+
+
+
+	/**
+	 * Test specific get replaying with 2 entries.
+	 * Requests are made using HTTP POST
+	 */
+	public function test_valid_using_post()
+	{
+		$streamid = "teststream";
+		$e1content = "{\"name\":\"entry1\",\"int\":1}";
+		$e2content = "{\"name\":\"entry2\",\"int\":2}";
+
+		//
+		// add entry 1
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e1content);
+		$add->send();
+
+		//
+		// add entry 2
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e2content);
+		$add->send();
+
+
+		/**
+		 * lets start by incrementing the stream position
+		 **/
+
+		//
+		// get entry 1
+		//
+		$e1 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_POST);
+		$e1->send();
+		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for the first entry is wrong");
+
+		//
+		// get entry 2
+		//
+		$e2 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_POST);
+		$e2->send();
+		$this->assertEquals($e2content, $e2->getResponseBody(), "The response for the second entry is wrong");
+
+
+		/**
+		 * now let us get specific entries
+		 **/
+
+		//
+		// get entry 2
+		//
+		$e2 = new HttpRequest(get_endpoint("/$streamid/2"), HttpRequest::METH_POST);
+		$e2->send();
+		$this->assertEquals($e2content, $e2->getResponseBody(), "The response for entryid 2 is wrong");
+
+		//
+		// get entry 1
+		//
+		$e1 = new HttpRequest(get_endpoint("/$streamid/1/"), HttpRequest::METH_POST);
+		$e1->send();
+		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for entryid 1 is wrong");
+	}
 }
 
 ?>
