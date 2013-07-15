bd172d6dae60b59a9dd31794d0bf8a1a25c18142
Patrick <pingvinen@isflagen.dk>
2013-07-15 13:06:36 +0200

    Added a test for simple getting with 2 interleaved streams

--- src/replayer-tests/getters/SimpleGetTest.php
+++ src/replayer-tests/getters/SimpleGetTest.php
@@ -181,6 +181,83 @@ class SimpleGetTest extends FixtureBase
 		$this->assertEquals(200, $e1->getResponseCode(), "The response code should have been 200");
 		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for the first entry is wrong");
 	}
+
+
+
+
+	/**
+	 * Test simple get replaying with 2 entries with
+	 * 2 streams having interleaved entries
+	 * Requests are made using HTTP GET
+	 */
+	public function test_interleaved_streams_using_get()
+	{
+		$streamid = "teststream";
+		$e1content = "{\"name\":\"entry1\",\"int\":1}";
+		$e2content = "{\"name\":\"entry2\",\"int\":2}";
+
+		$otherstreamid = "otherstream";
+		$othere1content = "{\"name\":\"other entry1\",\"int\":1}";
+		$othere2content = "{\"name\":\"other entry2\",\"int\":2}";
+
+		//
+		// add entry 1.1
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e1content);
+
+		$add->send();
+
+
+		//
+		// add entry 2.1
+		//
+		$add = new HttpRequest(get_endpoint("/add/$otherstreamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($othere1content);
+
+		$add->send();
+
+
+		//
+		// add entry 1.2
+		//
+		$add = new HttpRequest(get_endpoint("/add/$streamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($e2content);
+
+		$add->send();
+
+
+		//
+		// add entry 2.2
+		//
+		$add = new HttpRequest(get_endpoint("/add/$otherstreamid/"), HttpRequest::METH_POST);
+		$add->addRawPostData($othere2content);
+
+		$add->send();
+
+
+
+		//
+		// get entry 1.1
+		//
+		$e1 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_GET);
+		$e1->send();
+		$this->assertEquals($e1content, $e1->getResponseBody(), "The response for the first entry is wrong");
+
+		//
+		// get entry 1.2
+		//
+		$e2 = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_GET);
+		$e2->send();
+		$this->assertEquals($e2content, $e2->getResponseBody(), "The response for the second entry is wrong");
+
+		//
+		// get last entry (should be entry 1.2)
+		//
+		$last = new HttpRequest(get_endpoint("/$streamid/"), HttpRequest::METH_GET);
+		$last->send();
+		$this->assertEquals($e2content, $last->getResponseBody(), "The response for the last entry is wrong");
+	}
 }
 
 ?>
