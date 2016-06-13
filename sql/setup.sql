e99e2ce0899c33077bfb7044067f391332b9ef1f
Nicklas Laine Overgaard <nicklas@isharp.dk>
2016-06-13 16:28:18 +0200

    Only create database and table if they don't already exist

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,14 +1,14 @@
 #
 # create the database
 #
-CREATE DATABASE `webreplay` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `webreplay` /*!40100 DEFAULT CHARACTER SET utf8 */;
 use `webreplay`;
 
 
 #
 # the streams
 #
-CREATE  TABLE `streams` (
+CREATE  TABLE IF NOT EXISTS `streams` (
   `id` varchar(255) NOT NULL COMMENT 'The ID of the stream' ,
   `description` TEXT NOT NULL DEFAULT '' COMMENT 'A description of the stream' ,
   `position` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The current position in the stream (i.e. the ID of the entry sent last)' ,
@@ -20,7 +20,7 @@ COMMENT = 'Meta-data for streams';
 #
 # the stream entries
 #
-CREATE  TABLE `entries` (
+CREATE  TABLE IF NOT EXISTS `entries` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The entry ID' ,
   `stream_id` varchar(255) NOT NULL COMMENT 'The ID of the stream that this entry belongs to' ,
   `content` TEXT NOT NULL COMMENT 'The body of the request' ,
@@ -32,12 +32,3 @@ CREATE  TABLE `entries` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 COMMENT = 'The entries of the streams';
-
-
-
-#
-# the user
-#
-create user 'webreplay'@'localhost' identified by '123456';
-grant delete,insert,update,select,drop on webreplay.* to 'webreplay'@'localhost';
-flush privileges;
